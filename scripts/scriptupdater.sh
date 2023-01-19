eb75cff21c9190d630ed6481febc9a0c7597ff79
Jon Stricoff <stricoff92@gmail.com>
2023-01-19 14:38:58 -0500

    wip

--- scripts/scriptupdater.sh
+++ scripts/scriptupdater.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 
 echo "********** updating PYHELP **********"
-curl https://raw.githubusercontent.com/HunterCSIT/server-docs/main/scripts/pyhelp.sh > /usr/local/bin/pyhelp
+curl -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache"  -H "Expires: 0" https://raw.githubusercontent.com/HunterCSIT/server-docs/main/scripts/pyhelp.sh > /usr/local/bin/pyhelp
 chown root:root /usr/local/bin/pyhelp
 chmod 755 /usr/local/bin/pyhelp
